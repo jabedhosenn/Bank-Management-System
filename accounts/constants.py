85ca0caee777027cf1e52d617edc44b66d2e8b83
Jabed <jabedhosenn@gmail.com>
2025-05-26 02:22:14 +0600

    first commit

---
+++ accounts/constants.py
@@ -0,0 +1,12 @@
+ACCOUNT_TYPE = (
+    ('Select', 'Select'),
+    ('Savings', 'Savings'),
+    ('Current', 'Current'),
+    ('Fixed', 'Fixed Deposit'),
+)
+GENDER_TYPE = (
+    ('Select', 'Select'),
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Other', 'Other'),
+)
